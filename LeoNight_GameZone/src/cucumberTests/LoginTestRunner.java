7330b6e1d134fe46ee0b098d5087d972c40c1798
Miiiii <Miiiii@10.0.0.199>
2018-10-01 14:51:37 -0400

    My test code

---
+++ LeoNight_GameZone/src/cucumberTests/LoginTestRunner.java
@@ -0,0 +1,11 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features", glue="stepImplementation")
+public class LoginTestRunner {
+
+}
